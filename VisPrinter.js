eeae43e18aa59bc9386a38facc2a506adb8a172b
dronus <paul.geisler@web.de>
2012-12-20 13:42:12 +0100

    cleanup and comments

--- VisPrinter.js
+++ VisPrinter.js
@@ -1,5 +1,5 @@
 
-
+// get a single cookie from this domains cookies
 function getCookieValue(cookieName)
 {
  var value=null;
@@ -23,16 +23,23 @@ function getCookieValue(cookieName)
 }
 
 
-
+// the main reprap printer UI controller
 VisPrinter=new function(){
 
+	// if the server is currently connected to a printer
+	this.connected=false;
+
+	// the mesh currently previewed, if any
+	this.mesh=null; 
+
+	// load a .stl or .gcode file for preview
 	this.load=function(file){
 		document.getElementById('slicingStyle').innerHTML='.sliced{visibility: hidden;}';
 		var reader=new FileReader();
 		reader.onload=function(e){
 			var text=e.target.result;
 			var suffix=file.name.substring(file.name.lastIndexOf('.'));
-			if     (suffix=='.stl'  ) VisPrinter.parseStl(text);
+			if	 (suffix=='.stl'  ) VisPrinter.parseStl(text);
 			else if(suffix=='.gcode') {
 				VisPrinter.uploadGcode(text);
 				VisPrinter.onSliced(text);
@@ -45,8 +52,7 @@ VisPrinter=new function(){
 		reader.readAsText(file);
 	}
 
-	this.mesh=null; 
-
+	// build a mesh from given polygons for 3d preview
 	this.buildMesh = function(polygons) {
 
 		var mesh = new GL.Mesh({ normals: true, colors: true });
@@ -69,6 +75,7 @@ VisPrinter=new function(){
 		return mesh;
 	};
 
+	// parse a .stl file for 3d preview
 	this.parseStl=function(stl){
 
 		this.stl=stl;
@@ -101,68 +108,78 @@ VisPrinter=new function(){
 		this.update();
 	}
 
-    this.httpPost=function(path,data, callback){
-            var req = new XMLHttpRequest();
-            req.overrideMimeType ( "text / plain"); 
-            req.onreadystatechange = function () {
-                if (req.readyState == 4) {
-                            if(req.status!=200) {
-                                    alert("POST failed in "+path+", server response:\n"+req.responseText);
-                            }
-                    callback(req.responseText);
-                }
-            };
-    	var formData=new FormData();
-    	for(key in data)
+	// issue a http POST request and call callback on response
+	this.httpPost=function(path,data, callback){
+		var req = new XMLHttpRequest();
+		req.overrideMimeType ( "text / plain"); 
+		req.onreadystatechange = function () {
+			if (req.readyState == 4) {
+				if(req.status!=200) {
+						alert("POST failed in "+path+", server response:\n"+req.responseText);
+				}
+				callback(req.responseText);
+			}
+		};
+		var formData=new FormData();
+		for(key in data)
 		formData.append(key, data[key]);
 
-            req.open('POST',path, true);
-            req.send(formData);
-    }
+		req.open('POST',path, true);
+		req.send(formData);
+	}
 
-    this.httpGet=function(path,callback){
-            var req = new XMLHttpRequest();
-            req.overrideMimeType ( "text / plain"); 
-            req.onreadystatechange = function () {
-                if (req.readyState == 4) {
-/*                            if(req.status!=200) {
-                                    alert("GET failed in "+path+", server response:\n"+req.responseText);
-                            }*/
-                    callback(req.responseText);
-                }
-            };
-        req.open('GET',path, true);
-        req.send(null);
-    }
+	// issue a http GET request and call callback on response
+	this.httpGet=function(path,callback){
+		var req = new XMLHttpRequest();
+		req.overrideMimeType ( "text / plain"); 
+		req.onreadystatechange = function () {
+			if (req.readyState == 4) {
+				callback(req.responseText);
+			}
+		};
+		req.open('GET',path, true);
+		req.send(null);
+	}
 
-
+	// set progress indicator to given value and caption
+	// a value of 0 (nothing in progress) and 1 (completed) hides the indicator.
 	this.progress=function(caption, p){
 
-		var indic         =document.getElementById('progress');
-		var bar           =document.getElementById('progressBar');
+		var indic		 =document.getElementById('progress');
+		var bar		   =document.getElementById('progressBar');
 		var captionElement=document.getElementById('progressName');
 
 		if(!caption) caption='';
 		captionElement.innerHTML=caption;
 
 		if (!p || p==1) indic.style.display='none';
-		else            indic.style.display='block';
+		else			indic.style.display='block';
 		if(p==1) p=0;
 		if(p<0.02) p=0.02;
 		bar.style.width=(p*100)+'%';
 	}
 
+	// poll /state for UI feedback of printer and slicing state
+	this.checkState=function(){
+			VisPrinter.httpGet('state',function(result){
+				VisPrinter.onState(result);
+			});
+			window.setTimeout(function(e){VisPrinter.checkState();},500);
+	}
+
+	// callback to handle a state report from the server
 	this.onState=function(response){
 		this.state=JSON.parse(response);
 		
+		// set progress indicator by ongoing server processes
 		this.onProgress(this.state.progress);
 
 		// all boolean state properties are reflected by a visibility CSS class 
-		// named .property or .not_property that can be used to show UI elements 
+		// named .property or .not_property that can be used to show/hide UI elements 
 		// depending on state
 		var stateStyle="";
 		for(key in this.state)
-			if      (this.state[key]===false)
+			if	  (this.state[key]===false)
 				stateStyle+="."+key+"{visibility: hidden;}\n"
 			else if (this.state[key]===true ) 
 				stateStyle+=".not_"+key+"{visibility: hidden;}\n";
@@ -170,6 +187,7 @@ VisPrinter=new function(){
 		if (stateStyle!=stateElement.innerHTML) stateElement.innerHTML=stateStyle;
 	}
 
+	// handle a progress change
 	this.onProgress=function(response){
 		if(!response) response='Idle 0';
 		var parts=response.split(' ');
@@ -179,6 +197,10 @@ VisPrinter=new function(){
 		this.progress(caption, progress/100);
 	}
 
+	// invoke the slicer.
+	// first, the .stl to be sliced is uploaded to the server
+	// second, if the upload completes, it's callback invokes the slicer
+	// third, if the slicer completes, it's callback calls onScliced(...)
 	this.slice=function(){
 		if(!this.stl){
 			alert("Nothing to slice. Load some .stl first.");
@@ -186,68 +208,91 @@ VisPrinter=new function(){
 		}
 
 		var config=document.getElementById('config').value;
+
+		// create upload completed callback to invoke slicer
 		var onUploaded=function(response){
 			this.console.value+="\nSlicing...\n";
 			VisPrinter.httpGet('slic3r?config='+config,function(response){
+				// slicing completed callback
 				VisPrinter.console.value+="\nSlicing complete.\n";
 				VisPrinter.onSliced(response)
 			});
 		}
 
+		// issue request to upload .stl
 		this.httpPost('upload',{'stl':this.stl}, onUploaded);
 		VisPrinter.progress("Uploading stl...",.5);
 		this.console.value+="\nUploading stl...\n";
 	}
+	
+	// upload a ready made .gcode 
 	this.uploadGcode=function(text){
+		// create callback to give feedback and complete progress indicator
 		var onUploaded=function(response){
 			VisPrinter.console.value+="\nUploaded.\n";
 			VisPrinter.progress();
 		}
 
+		// issue .gcode upload request
 		this.httpPost('upload',{'gcode':text}, onUploaded);
 		this.console.value+="\nUploading gcode...\n";
 		VisPrinter.progress("Uploading gcode...",.5);
 	}
 
+	// show edit.html for the currently selected config
 	this.editConfig=function(){
 		var config=document.getElementById('config').value;
 		window.open('edit.html?'+config);
 	}
 	
+	// issue G1 gcode to send printer to location given by goto form
 	this.goto=function(form){
-        var cmd="G1 X"+form.X.value+" Y"+form.Y.value+" Z"+form.Z.value+" E"+form.E.value;
-        this.cmd(cmd);
+		var cmd="G1 X"+form.X.value+" Y"+form.Y.value+" Z"+form.Z.value+" E"+form.E.value;
+		this.cmd(cmd);
 	}
 
+	// get session id from cookie
 	this.getSession=function(){
 		return getCookieValue("session");
 	}
 
+	// issue print command 
 	this.print=function(){
 		var session=this.getSession();
 
 		this.console.value+="\nPrinting...\n";
+		// issue pronsole command
 		this.cmd("load tmp/"+session+".gcode\nprint");
 	}
 	
+	// issue pronsole command, calling callback on completion
+	// only native pronsole commands like connect, pause etc. give a valid response
+	// as printer comamnds like gcodes are handled in background and their responses
+	// must be read by fetching the printer log at /printer
 	this.cmd=function(cmd,callback){
-	    var console=document.getElementById('console');
-	    if(!callback) console.value+="\n>"+cmd+"\n";
-	    if(!callback) callback=function(response){VisPrinter.onCmd(response)};
+		var console=document.getElementById('console');
+		if(!callback) console.value+="\n>"+cmd+"\n";
+		if(!callback) callback=function(response){VisPrinter.onCmd(response)};
 		this.httpGet('pronsole?cmd='+encodeURIComponent(cmd), callback);
 	}
 	
+	// default callback for pronsole commands 
+	// append the result to console textarea
 	this.onCmd=function(result){
 		var console=document.getElementById('console');
 		console.value+=result;
 		console.scrollTop = console.scrollHeight;
 	}
 		
+	// slicing completed handler
+	// the received gcode is parsed and a line mesh is build to preview the printer pathes
 	this.onSliced=function(gcode){
 		this.gcode=gcode;
 
+		// create a line mesh
 		var mesh = new GL.Mesh({ triangles: false, lines: true, colors: true });
 
+		// parse gcode and add lines
 		var pos={'X':0.0, 'Y':0.0, 'Z':0.0};
 		var lines=gcode.split("\n");
 		var index=0;
@@ -272,106 +317,102 @@ VisPrinter=new function(){
 		}
 		mesh.compile();
 		this.mesh=mesh;
+		// show mesh
 		this.update();
 		document.getElementById('slicingStyle').innerHTML='';
 	}
 	
-	this.connected=false;
+	// establish connection to printer
 	this.connect=function(){
-//	    document.getElementById('connection').innerHTML='connecting...';
-	    this.cmd('connect');            
+		// issue pronsole connect command
+		this.cmd('connect');			
 	}
-	
+
+	// repeatedly poll printer output buffer from server
+	// this is done every second if a printer is connected, and every 10 seconds
+	// if not, as connecting takes about 10s to succeed. 
+	// TODO it would be better to let the server retry the printer connection, or
+	// to retry connect indipendently of the check polling so we can poll the state 
+	// as often we like to stay responsive for printerless use (eg. slicing).
 	this.check=function(){
-            VisPrinter.httpGet('printer',function(result){
-                VisPrinter.onCheck(result);
-            });
+	
+		// issue /check request, calling onCheck on response
+		VisPrinter.httpGet('printer',function(result){
+			VisPrinter.onCheck(result);
+		});
 
-	    if(!this.connected) {
-	        this.connect();
-	        window.setTimeout(function(e){VisPrinter.check();},10000);
-	    }
-            else
-                window.setTimeout(function(e){VisPrinter.check();},1000);
+
+		if(!this.connected) {
+			// not connected to the printer, try to connect and check again in 10s
+			this.connect();
+			window.setTimeout(function(e){VisPrinter.check();},10000);
+		}
+		else
+			// the printer is connected, check again in one second
+			window.setTimeout(function(e){VisPrinter.check();},1000);
 	}
 
-        this.onCheck=function(result){
-            var lines=result.split('\n')            
-            for(var i=0; i<lines.length; i++){
-                var line=lines[i];
-                if(!line) continue;
-                if(line.indexOf('ok ')==0) {
-			this.connected=true;
-			    document.getElementById('connection'     ).innerHTML='connected';
-			    document.getElementById('connectionStyle').innerHTML='';
+	// printer output response handler
+	// refreshes the this.connected state and corresponding UI
+	// adds messages to the console textarea
+	this.onCheck=function(result){
+		var lines=result.split('\n')			
+		for(var i=0; i<lines.length; i++){
+			var line=lines[i];
+			if(!line) continue;
+			if(line.indexOf('ok ')==0) {
+				// we received an 'ok', so consider the printer connected
+				this.connected=true;
+				document.getElementById('connection'	 ).innerHTML='connected';
+				document.getElementById('connectionStyle').innerHTML='';
+			}
+			// echo printer output to console textarea
+			this.console.value+=line+"\n";
 		}
-		this.console.value+=line+"\n";
-            }
-        }
-
-	this.checkState=function(){
-            VisPrinter.httpGet('state',function(result){
-                VisPrinter.onState(result);
-            });
-            window.setTimeout(function(e){VisPrinter.checkState();},500);
 	}
 
-	this.hashchange=function(data)
-	{
-		if(this.justSaved) {
-			this.justSaved=false;
-			return;
-		}
-		if(document.location.hash)
-			data=document.location.hash.substring(1);
-		if(data){
-			this.sceneTree=JSON.parse(data);
-			this.convertLegacy(this.sceneTree);
-			this.csgWorker.csgCache={};
-		}
-		this.updateTreeView();
-		this.update();
+	// /config response handler
+	// fills in the config selector dropdown
+	this.onConfigs=function(response){
+		var configs=response.split("\n");
+		var select=document.getElementById('config');
+		for(var i=0; i<configs.length; i++){
+			var config=configs[i];
+			if(!config) continue;
+			var option=document.createElement('option');
+			option.value=config;
+			option.innerHTML=config.substr(config.indexOf('/')+1);
+			select.add(option);
+		}	
 	}
 
-    this.onConfigs=function(response){
-        var configs=response.split("\n");
-        var select=document.getElementById('config');
-        for(var i=0; i<configs.length; i++){
-            var config=configs[i];
-            if(!config) continue;
-            var option=document.createElement('option');
-            option.value=config;
-            option.innerHTML=config.substr(config.indexOf('/')+1);
-            select.add(option);
-        }    
-    }
-
+	// issue printer pause command
 	this.pause=function(){
 		this.cmd("pause");
 	}
+	
+	// issue printer resume command
 	this.resume=function(){
 		this.cmd("resume");
 	}
 
+	// request server /cancel 
+	// this cancels the current operation, that may be a slicing or printing operation
 	this.cancel=function(){
-            this.httpGet('cancel');
+			this.httpGet('cancel');
 	}
 	
+	// update the 3d preview to show our current mesh
 	this.update=function(){
-		//viewer.mesh=viewer.buildMesh(this.csgWorker.getPolygons(this.sceneTree,'root'));
 		viewer.mesh=this.mesh;
+		// adjust camera to show anything
 		viewer.showAll();
 		viewer.gl.ondraw();
 	}
 
-	this.keypress=function(e){
-		if(e.target.tagName=='INPUT') return;
-		if(e.charCode=='x'.charCodeAt(0)) this.cut();
-		if(e.charCode=='c'.charCodeAt(0)) this.copy();
-		if(e.charCode=='v'.charCodeAt(0)) this.paste();
-		if(e.charCode=='y'.charCodeAt(0)) this.deselect();
-	};
-
+	// attach this VisPrinter controller to a HTML UI
+	// the html need to provide several UI elements and classes
+	// see index.html 
 	this.attach=function(){
 		var viewPane=document.getElementById('view');
 		if(viewPane) {
@@ -380,14 +421,9 @@ VisPrinter=new function(){
 		}
 		this.console=document.getElementById('console');
 		var VisPrinter=this;
-		window.addEventListener('keypress', function(e){VisPrinter.keypress(e)} ,false);
 		this.check();
 		this.checkState();
 		this.httpGet('configs',function(response){VisPrinter.onConfigs(response)});
-//		window.addEventListener("hashchange", function(e){VisPrinter.hashchange(e)}, false);
-//		this.hashchange();
-		
-
 	}
 }
 
--- webserver.py
+++ webserver.py
@@ -37,6 +37,10 @@ printer.recvlisteners.append(recv_printer)
 # server request handler
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
+    # serve the printer's output
+    # the output is kept at the recv_buffer and deleted after serving
+    # thus the client only receives every message once.
+    # TODO what if more then one client connects?
     def serve_printer(self):
         global recv_buffer
         self.send_response(200)
@@ -46,6 +50,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         for line in tmp_buffer:
         	self.wfile.write(line)
 
+    # serve a file from our folder
     def serve_file(self,url_path):
         # map URL path to file in our directory
         file_path=os.path.abspath('./'+url_path)
@@ -65,6 +70,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.wfile.write(f.read())
         f.close()
 
+    # serve a list of all configs in /configs folder
     def serve_configs(self):
         # send headers
         self.send_response(200)
@@ -85,6 +91,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         except Exception as e:
             print e
 
+    # run slicer and return gcode
     def serve_slic3r(self,session_id,config):
 	global progress
 	progress="Slicing... 1"
@@ -97,6 +104,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 	self.wfile.write(gcode)
 	progress="Slicing... 100"
 
+    # cancel any ongoing operations: printing and slicing
     def serve_cancel(self, session_id):
 	global progress 
 	for process in processes:
@@ -105,6 +113,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 	if printer.p.printing:
 		printer.onecmd('pause')
     
+    # serve some state for UI feedback
     def serve_state(self,session_id):
 	global progress
         self.send_response(200)
@@ -112,20 +121,26 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 	if printer.p.printing:
 		progress="Printing... "+str(int(99*float(printer.p.queueindex)/len(printer.p.mainqueue))+1)
 	state={
-		'online':printer.p.online,
-		'printing': printer.p.printing,
-		'paused':printer.p.paused,
-		'clear':printer.p.clear,
-		'progress':progress
+		'online':printer.p.online,        # if the printer is connected
+		'printing': printer.p.printing,   # if the printer is currently printing
+		'paused':printer.p.paused,        # if the printer is currently paused
+		'clear':printer.p.clear,          # if the printer is clear to print
+		'progress':progress               # progress of current slicing or printing operation
 	}
 	_json=json.dumps(state, indent=4)
 	self.wfile.write(_json)
  
+    # parse a line of slic3r's stdout output and set progress indicator accordingly
+    # currently only the 'filling layer' phase is tracked, that usually makes up most of the slicing time. 
+    # TODO can we provide an estimation of progress at the earlier 'geometry' phase too?
     def monitor_slic3r(self,line):
         global progress
+        # find number of layers to do. this is first mentioned as the last 'Making surfaces ...' output
         match=re.match('Making surfaces for layer ([0-9]+)',line)
         if match:
             self.slic3r_layers=int(match.group(1))
+        # find actual 'Filling layer' message, indicating the most costly operations's progress
+        # and set progress indicator
         match=re.match('Filling layer ([0-9]+)',line)
         if match:
             progress="Slicing... "+str(30+int(match.group(1))*70/self.slic3r_layers)
@@ -140,13 +155,15 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 	# run the command
 	process=subprocess.Popen(cmdline.split(' '),stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 	processes.append(process)
+        # monitor command's outputs and invoke callback for each line
 	while process.poll()==None:
 		line=process.stdout.readline()
 		if not line: break
 		callback(line)
 	if(process in processes):
 		processes.remove(process)
-	
+
+    # save given data to file name
     def save_tmp(self,name,content):
        # copy content to file
        print 'Saving to ',name
@@ -154,6 +171,8 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
        f.write(content)
        f.close()
 
+    # save uploaded data to /tmp folder
+    # the files are named by the session_id and their field name as suffix.
     def save_uploads(self, session_id):
         contenttype_header=self.headers.getheader('content-type',False)
         if not contenttype_header:
@@ -174,6 +193,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 filename='tmp/'+session_id+'.'+key;
             self.save_tmp(filename, contents[0])
 
+    # get session id from cookie
     def get_session(self):
         id=False
         if "Cookie" in self.headers:
@@ -184,12 +204,22 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 raise Exception('Invalid session cookie')
         return id        
 
+    # handle GET request by general serve_request()
     def do_GET(self):
         self.serve_request()
         
+    # handle POST request by general serve_request()
     def do_POST(self):
         self.serve_request()
         
+    # handle incoming request
+    # 
+    # provides several functions on the pathes: 
+    # /pronsole, /configs, /printer, /slic3r, /state, /cancel, /upload.
+    # see the corresponding serve_...() functions for details.
+    #
+    # for every other path, serve file from local directory. 
+    # 
     def serve_request(self):
         try:
             # split URL parts (path, querystring)
@@ -226,13 +256,17 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 class ThreadingServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
     pass
 
-if __name__ == '__main__':
-    try:
-        os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
-        server = ThreadingServer(('', settings.port), RequestHandler)
-        #server = BaseHTTPServer.HTTPServer(('', settings.port), RequestHandler)
-        print 'Server running on port '+str(settings.port)
-        server.serve_forever()
-    except KeyboardInterrupt:
-        server.socket.close()
+try:
+    # make sure the current working dir is the one of this script (we serve files from it!)
+    os.chdir(os.path.dirname(os.path.abspath(sys.argv[0]))) 
+    # create server 
+    server = ThreadingServer(('', settings.port), RequestHandler) # multi threaded server
+    #server = BaseHTTPServer.HTTPServer(('', settings.port), RequestHandler)  # single threaded server
+    print 'Server running on port '+str(settings.port)
+    # and run it...
+    server.serve_forever()
+except KeyboardInterrupt:
+    # on ctrl+c try to close cleanly
+    # TODO this doesn't always seem to work, especially if stdout from pronsole is redirected it seems
+    server.socket.close()
 
